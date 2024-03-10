efd924e605608e1fb9b314451064a7f5e19bd04c
Alexandru <alexandrudorobat@gmail.com>
2024-03-10 15:18:11 +0100

    Prueba

---
+++ app/Models/Funko.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Funko extends Model
+{   
+    use HasFactory;
+}
